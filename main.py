287758ea0fe91745ddb21631f81ce27b83e060ba
itGabriel <gabriel.gustavo.s@hotmail.com>
2020-05-18 18:11:41 -0300

    updating

--- main.py
+++ main.py
@@ -38,54 +38,33 @@ def index():
 
 @app.route('/regiao')
 def regiao():
-    dados = []
-    verificador_provincia = False
     message = None
+    dados_comProvincia = []
+    dados_semProvincia = []
     try:
         regiao = request.args['regiao']
         busca = apiCovid.por_regiao(regiao)
         if busca[1] == 200:
             pais = busca[0].json()
-            dados_segurado = None
+            # dados_segurado = None
             for chave in pais:
-                if dados_segurado == None:
-                    dados_segurado = chave
-                
+                json = {
+                    'pais': chave['Country'],
+                    'provincia': chave['Province'],
+                    'codigo': chave['CountryCode'],
+                    'total_confirmados': util.custom_numero(chave['Confirmed']),
+                    'total_mortos': util.custom_numero(chave['Deaths']),
+                    'total_recuperados': util.custom_numero(chave['Recovered']),
+                    'data': util.custom_data(chave['Date']),
+                    }
                 if chave['Province']:
-                    verificador_provincia = True
-                
-                if dados_segurado != None:
-                    if dados_segurado['Date'].split('T')[0] == chave['Date'].split('T')[0]:
-                        if chave['Confirmed'] > dados_segurado['Confirmed']:
-
-                            json = {
-                            'pais': chave['Country'],
-                            'provincia': chave['Province'],
-                            'codigo': chave['CountryCode'],
-                            'total_confirmados': util.custom_numero(chave['Confirmed']),
-                            'total_mortos': util.custom_numero(chave['Deaths']),
-                            'total_recuperados': util.custom_numero(chave['Recovered']),
-                            'data': util.custom_data(chave['Date']),
-                            }
-                            dados.append(json)
-                    else:
-                        json = {
-                            'pais': chave['Country'],
-                            'provincia': chave['Province'],
-                            'codigo': chave['CountryCode'],
-                            'total_confirmados': util.custom_numero(chave['Confirmed']),
-                            'total_mortos': util.custom_numero(chave['Deaths']),
-                            'total_recuperados': util.custom_numero(chave['Recovered']),
-                            'data': util.custom_data(chave['Date']),
-                            }
-                        dados.append(json)
-                    
-    
+                    dados_comProvincia.append(json)
+                else:
+                    dados_semProvincia.append(json)
     except:
         message = "Falha em processar a busca dos dados"
     # dados.reverse()
-    
-    return render_template('tabela/por_regiao.html', dados=dados, provincia=verificador_provincia, message=message)
+    return render_template('tabela/por_regiao.html', dados_semProvincia=dados_semProvincia, dados_comProvincia=dados_comProvincia, provincia=verificador_provincia, message=message)
 
 
 app.run('127.0.0.1', 8000)
---
+++ teste.py
@@ -0,0 +1,5 @@
+total = 0
+arquivo = open('infos.json').read()
+print(arquivo[10])
+# for x in arquivo.:
+    # print(x)
